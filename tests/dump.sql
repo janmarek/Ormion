60ae43bb7268f350e385782bfc2ee8263177771b
Jan Marek <mail@janmarek.net>
2010-04-22 12:04:42 +0200

    updated sql dump

--- tests/dump.sql
+++ tests/dump.sql
@@ -1,4 +1,3 @@
--- Adminer 2.2.0 dump
 SET NAMES utf8;
 SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
@@ -13,18 +12,18 @@ CREATE TABLE `comments` (
   `mail` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `page` (`page`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`page`) REFERENCES `pages` (`id`)
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`page`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 
 DROP TABLE IF EXISTS `connections`;
 CREATE TABLE `connections` (
-  `commentId` int(10) unsigned NOT NULL,
+  `tagId` int(10) unsigned NOT NULL,
   `pageId` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`commentId`,`pageId`),
+  PRIMARY KEY (`tagId`,`pageId`),
   KEY `pageId` (`pageId`),
-  CONSTRAINT `connections_ibfk_1` FOREIGN KEY (`commentId`) REFERENCES `comments` (`id`),
-  CONSTRAINT `connections_ibfk_2` FOREIGN KEY (`pageId`) REFERENCES `pages` (`id`)
+  CONSTRAINT `connections_ibfk_3` FOREIGN KEY (`tagId`) REFERENCES `tags` (`id`),
+  CONSTRAINT `connections_ibfk_4` FOREIGN KEY (`pageId`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 
@@ -40,9 +39,19 @@ CREATE TABLE `pages` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
+
 DROP TABLE IF EXISTS `table`;
 CREATE TABLE `table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `order` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `url` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
